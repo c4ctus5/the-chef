e8cf56cf9a410a7a2679bd133ac764117e162773
c4ctus5 <cactusguy5@protonmail.com>
2021-12-11 20:46:08 +0100

    add basic logging

--- src/the-chef.py
+++ src/the-chef.py
@@ -6,13 +6,17 @@ import argparse
 
 from recipe import Recipe
 
-logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='the-chef.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
